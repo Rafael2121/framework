8431b7cdbd7dafab29d5bfbd22c7633d60c501bd
Rafael Milanez <rafaeldinoster@gmail.com>
2022-02-27 12:25:16 -0300

    add service and connection files

--- query/tests.py
+++ query/tests.py
@@ -1,3 +1,29 @@
+from http import client
+from unittest import skip
 from django.test import TestCase
+from .services import JsonPlaceholderService
+from .connection import JsonPlaceholderConnection
 
-# Create your tests here.
+
+class ServiceTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.service = JsonPlaceholderService()
+
+    def test_fetch_all(self):
+
+        result = self.service.fetch_all()
+
+        self.assertTrue(result)
+
+
+class ConnectionTestCase(TestCase):
+
+    def setUp(self) -> None:
+        self.conn = JsonPlaceholderConnection()
+
+    def test_get_result_should_be_a_list(self):
+        result = self.conn.get("todos")
+
+        self.assertEqual(list, type(result))
+
